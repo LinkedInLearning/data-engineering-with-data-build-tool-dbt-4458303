f471fe0d00b798bdb41237f22c3924cd1c09fcba
Mark Freeman <97306794+onthemarkdata@users.noreply.github.com>
2023-10-27 23:20:04 +0000

    chapter_07

---
+++ nyc_parking_violations/models/bronze/bronze_parking_violations.sql
@@ -0,0 +1,23 @@
+SELECT
+    summons_number,
+    registration_state,
+    plate_type,
+    issue_date,
+    violation_code,
+    vehicle_body_type,
+    vehicle_make,
+    issuing_agency,
+    vehicle_expiration_date,
+    violation_location,
+    violation_precinct,
+    issuer_precinct,
+    issuer_code,
+    issuer_command,
+    issuer_squad,
+    violation_time,
+    violation_county,
+    violation_legal_code,
+    vehicle_color,
+    vehicle_year,
+FROM
+    parking_violations_2023
