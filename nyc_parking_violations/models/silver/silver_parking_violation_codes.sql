74fc56b30a1122fb57103bfd9ff73f0637e34a43
ramuf <48536005+ramuf@users.noreply.github.com>
2024-02-24 17:35:53 +0000

    added github workflows

--- nyc_parking_violations/models/silver/silver_parking_violation_codes.sql
+++ nyc_parking_violations/models/silver/silver_parking_violation_codes.sql
@@ -6,17 +6,19 @@ WITH manhattan_violation_codes AS (
         manhattan_96th_st_below AS fee_usd,
     FROM
         {{ref('bronze_parking_violation_codes')}}
+        -- bronze_parking_violation_codes
 ),
 
-all_other_violation_codes AS (
-    SELECT
-        violation_code,
-        definition,
-        FALSE AS is_manhattan_96th_st_below,
-        all_other_areas AS fee_usd,
-    FROM
-        {{ref('bronze_parking_violation_codes')}}
-)
+  all_other_violation_codes AS (
+      SELECT
+          violation_code,
+          definition,
+          FALSE AS is_manhattan_96th_st_below,
+          all_other_areas AS fee_usd,
+      FROM
+          {{ref('bronze_parking_violation_codes')}}
+          -- bronze_parking_violation_codes
+  )
 
 SELECT * FROM manhattan_violation_codes
 UNION ALL
