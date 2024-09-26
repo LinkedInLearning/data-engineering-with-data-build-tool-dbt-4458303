40b55585968a8223f78296443288135cd5bcc240
Dickson Okorie <134509220+Dickson-Okorie@users.noreply.github.com>
2024-09-26 18:36:59 +0000

    added github workflows

---
+++ nyc_parking_violation/models/docs/docs_block.md
@@ -0,0 +1,5 @@
+# models/docs/docs_blocks.md
+
+{% docs violation_code %}
+The standardized code of the violation.
+{% enddocs %}
---
+++ nyc_parking_violation/tests/generic/generic_not_null.sql
@@ -0,0 +1,9 @@
+-- test/generic/generic_not_null.sql
+-- source: https://docs.getdbt.com/guides/best-practices/writing-custom-generic-tests#generic-tests-with-default-config-values
+{% test generic_not_null(model, column_name) %}
+
+    select *
+    from {{ model }}
+    where {{ column_name }} is null
+
+{% endtest %}
