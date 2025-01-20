29b0786a2bc1a0d0893c0bf2574ffdfc0ae42a56
MayymTethi <86873501+MayymTethi@users.noreply.github.com>
2025-01-20 12:55:47 +0000

    edit commit workflows and all other

---
+++ nyc_parking_violations/models/bronze/bronze_parking_violation_codes.sql
@@ -0,0 +1,8 @@
+SELECT
+    code AS violation_code,
+    definition,
+    manhattan_96th_st_below,
+    all_other_areas
+FROM
+    parking_violation_codes
+  
---
+++ nyc_parking_violations/models/example/ref_model.sql
@@ -0,0 +1,5 @@
+SELECT
+  COUNT(*)
+FROM  
+  {{ref('first_model')}}
+  
