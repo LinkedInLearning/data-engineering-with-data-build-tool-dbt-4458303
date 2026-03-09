207a46b498841f4b649363ca526d7d32db267e57
solomonericj <solomon.eric.j@gmail.com>
2026-03-09 19:24:35 +0000

    added github wfs

--- nyc_parking_violations/models/gold/gold_ticket_metrics.sql
+++ nyc_parking_violations/models/gold/gold_ticket_metrics.sql
@@ -3,7 +3,7 @@ SELECT
     COUNT(summons_number) AS ticket_count,
     SUM(fee_usd) AS total_revenue_usd
 FROM
-    {{ref('silver_violation_tickets')}}
+    {{ref('silver_violations_tickets')}}
 GROUP BY
     violation_code
 ORDER BY
