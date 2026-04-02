e50a68f9c0c7caf5ccffcebc0e3bb7e9a6ee9461
Kre8tor <108299479+Kre8tor@users.noreply.github.com>
2026-04-02 11:20:21 +0000

    Added Github workflows

--- nyc_parking_violations/tests/generic/generic_not_null.sql
+++ nyc_parking_violations/tests/generic/generic_not_null.sql
@@ -1,3 +1,4 @@
+-- test/generic/generic_not_null.sql
 -- source: https://docs.getdbt.com/guides/best-practices/writing-custom-generic-tests#generic-tests-with-default-config-values
 {% test generic_not_null(model, column_name) %}
 
